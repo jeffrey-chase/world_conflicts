e73a669ee17faa3e36defe1fd8aa396dcedb8a23
Jeffrey Chase <jc7437a@student.american.edu>
2019-04-14 11:21:30 -0400

    updates

---
+++ scripts/graph.js
@@ -0,0 +1,184 @@
+width = 900;
+height = 500;
+
+let svg_conflict = d3.select('#conflict-graph')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+
+let simulation = d3.forceSimulation().alpha(0.5)
+  .force('link', d3.forceLink().id((d) => {
+    return d.id;
+  }))
+  .force('charge', d3.forceManyBody().strength(-15))
+  .force('center', d3.forceCenter(width / 2, height / 2))
+
+let labSimulation = d3.forceSimulation().alpha(0.5)
+  .force('link', d3.forceLink().id((d) => {
+    return d.id;
+  }))
+  .force('charge', d3.forceManyBody().strength(-15))
+  .force('center', d3.forceCenter(width / 2, height / 2))
+
+let graph = function (data) {
+  let alliances = data[0];
+  let codeConverter = data[1];
+  console.log(codeConverter);
+  let conflicts = data[2];
+  let countries = data[3];
+  let allLinks = data[4]
+
+  let linkSize = d3.scaleLog()
+    .domain(d3.extent(conflicts.map((d) => {
+      return +d.value
+    })))
+    .range([0.5, 5]);
+
+  let nodeColor = d3.scaleLog()
+    .domain(d3.extent(conflicts.map((d) => {
+      return +d.value
+    })))
+    .range([0, 1]);
+
+  function getLinkColor(d) {
+    return d3.interpolatePlasma(linkColor(+d.value));
+  }
+
+  function getLinkSize(d) {
+    return linkSize(+d.value);
+  }
+
+
+
+
+  //  let links = svg_conflict.append('g')
+  //    .attr('class', 'links')
+  //    .selectAll("path.conflict")
+  //    .data(conflicts)
+  //    .enter().append('path')
+  //    .attr('class', 'conflict')
+  //    .attr('fill', 'none')
+  //    .attr('stroke-width', getLinkSize)
+  //    .attr('stroke', '#ff9999')
+  //    .style('opacity', 0)
+
+
+  let links = svg_conflict.append('g')
+    .attr('class', 'links')
+    .selectAll("line.conflict")
+    .data(conflicts)
+    .enter().append('line')
+    .attr('class', 'conflict')
+    .attr('fill', 'none')
+    .attr('stroke-width', getLinkSize)
+    .attr('stroke', '#ff0000')
+    .style('opacity', 0)
+
+  links.transition()
+    .style('opacity', 1)
+    .attr('stroke', '#dd8833')
+    .delay(function (d, i) {
+      return i * 10;
+    })
+    .duration(2000);
+
+
+
+  let nodes = svg_conflict.append('g')
+    .attr('class', 'nodes')
+    .selectAll("g.nodes")
+    .data(countries)
+    .enter().append("g");
+
+
+  let circles = nodes.append('circle')
+    .attr('r', 5)
+    .attr('fill', '#333344')
+    .attr('stroke', '#80808A')
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended)
+      )
+
+
+  let labels = nodes.append('text')
+    .attr('class', 'graph-labels')
+    .text(function (d) {
+      try {
+        console.log(codeConverter[d.id]['name']);
+        return codeConverter[d.id]['name'];
+      } catch (err) {
+        return '';
+      };
+    })
+    .attr('x', 6)
+    .attr('y', 3);
+
+  function ticked() {
+    //    links
+    //      .attr("d", function (d) {
+    //        let dx = d.target.x - d.source.x,
+    //          dy = d.target.y - d.source.y,
+    //          dr = Math.sqrt(dx * dx + dy * dy);
+    //        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+    //      });
+
+    links
+      .attr("x1", function (d) {
+        return d.source.x;
+      })
+      .attr("y1", function (d) {
+        return d.source.y;
+      })
+      .attr("x2", function (d) {
+        return d.target.x;
+      })
+      .attr("y2", function (d) {
+        return d.target.y;
+      });
+
+
+
+    nodes
+      .attr("transform", function (d) {
+        d.x = Math.max(5, Math.min(width - 5, d.x));
+        d.y = Math.max(5, Math.min(height - 5, d.y))
+        return "translate(" + d.x + "," + d.y + ")";
+      })
+  }
+  simulation.nodes(countries).on('tick', ticked);
+  simulation.force('link').links(conflicts);
+
+
+    function dragstarted() {
+      if (!d3.event.active) simulation.alphaTarget(0.1).restart();
+      d3.event.subject.fx = d3.event.subject.x;
+      d3.event.subject.fy = d3.event.subject.y;
+      simulation.force('charge', d3.forceManyBody().strength(2))
+    }
+
+  function dragged(d) {
+    d3.event.subject.fx = d3.event.x;
+    d3.event.subject.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d3.event.subject.fx = null;
+    d3.event.subject.fy = null;
+    simulation.force('charge', d3.forceManyBody().strength(-15))
+
+  }
+
+};
+
+
+Promise.all([
+  d3.json('../data/formatted_data/alliances.json'),
+  d3.json('../data/formatted_data/ccode_converter.json'),
+  d3.json('../data/formatted_data/conflicts.json'),
+  d3.json('../data/formatted_data/nodes.json'),
+  d3.json('../data/formatted_data/combined_links.json')
+]).then((d) => graph(d));
