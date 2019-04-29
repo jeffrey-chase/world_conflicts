a4d77eb89d785a35bdb00694a136001b7c3d44d2
Jeffrey Chase <jc7437a@student.american.edu>
2019-04-28 20:04:14 -0400

    timeline prototype completed;

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -14,6 +14,13 @@
     let nested = d3.nest()
       .key((d) => +d.year)
       .sortKeys(d3.ascending)
+      .rollup((d) => {
+        console.log(d);
+        return {
+          major: d.filter((e) => e.intensity_level === "2"),
+          minor: d.filter((e) => e.intensity_level === "1")
+        };
+      })
       .entries(d);
 
     console.log(nested);
@@ -23,15 +30,20 @@
 
     let xScale = d3.scaleLinear()
       .domain(d3.extent(nested, (d) => d.key))
-      .range([10, width - 10]);
+      .range([100, width - 100]);
 
-    let yScale = d3.scaleLinear()
-      .domain([ d3.max(nested, (d) => d.values.length), 0])
-      .range([40, height - 20]);
+    let yScaleMinor = d3.scaleLinear()
+      .domain([0, d3.max(nested, (d) => d.value.minor.length)])
+      .range([height - 300, height - 5]);
+
+    let yScaleMajor = d3.scaleLinear()
+      .domain([d3.max(nested, (d) => d.value.major.length), 0])
+      .range([10, height - 350]);
 
     let svg = parent.append('svg')
       .attr('width', width)
       .attr('height', height);
+
 
     let points = svg.selectAll('circle.conflicts');
 
@@ -40,22 +52,82 @@
       let year = +el.key;
       let conflicts = el.values;
 
-      points.data(el.values).enter().append('circle')
-        .attr('class', 'circle')
+      points.data(el.value.minor).enter().append('circle')
+        .attr('class', 'conflicts')
+        .attr('r', 0.1)
+        .attr('fill', 'white')
+        .style('opacity', (d) => d.intensity_level === '2' ? 1.0 : 0.7)
         .attr('cx', xScale(year))
-        .attr('cy', (d, i) => {
-          return yScale(i);
+        .attr('cy', height+20)
+        .transition()
+        .delay((d, i) => {
+          return (year - 1946) * 90 + i * 2;
         })
+        .duration(2000)
+        .style('fill', (d) => d.intensity_level === '2' ? '#ff0000' : '#660000')
+        .style('stroke', (d) => d.intensity_level === '2' ? '' : '#ff0000')
         .attr('r', (d) => d.intensity_level === '2' ? 5 : 3)
-        .style('fill', (d) => d.intensity_level === '2' ? '#ff0000' : '#ff0000')
-        .style('opacity', (d) => d.intensity_level === '2' ? 1.0 : 0.3);
+        .attr('cy', (d, i) => {
+          return yScaleMinor(i);
+        })
+        .ease(d3.easePoly);
+
+      points.data(el.value.major).enter().append('circle')
+        .attr('class', 'conflicts')
+        .attr('r', 0.1)
+        .attr('fill', 'white')
+        .attr('cx', xScale(year))
+        .attr('cy', -10)
+        .transition()
+        .delay((d, i) => {
+          return (year - 1946) * 90 + i * 2;
+        })
+        .duration(2000)
+        .style('fill', (d) => d.intensity_level === '2' ? '#ff0000' : '#660000')
+        .style('stroke', (d) => d.intensity_level === '2' ? '' : '#ff0000')
+        .style('opacity', (d) => d.intensity_level === '2' ? 1.0 : 0.7)
+        .attr('r', (d) => d.intensity_level === '2' ? 5 : 3)
+        .attr('cy', (d, i) => {
+          return yScaleMajor(i);
+        })
+        .ease(d3.easePoly);
+      //        .ease(d3.easeBounceOut);
+      //        .ease(d3.easeCubicInOut);
     });
 
+    let axis = svg.append('g').attr('class', 'axis');
 
-    //      .attr('y', (d)=>yScale(+d.values[]));
+    axis.append('line')
+      .attr('class', 'axisline')
+      .attr('x1', 20)
+      .attr('x2', width - 100)
+      .attr('y1', 275)
+      .attr('y2', 275)
+      .attr('stroke', 'white');
 
+    tickData = d3.range(1945, 2017, 5);
+
+    let ticks = axis.selectAll('line.ticks')
+      .data(tickData).enter()
+      .append('g')
+
+    ticks
+      .append('line')
+      .attr('class', 'ticks')
+      .attr('x1', xScale)
+      .attr('x2', xScale)
+      .attr('y1', 270)
+      .attr('y2', 280)
+      .style('stroke', 'white');
+
+    ticks
+      .append('text')
+      .text((d) => d)
+      .attr('x', xScale)
+      .attr('y', 270)
+      .style('fill', 'white')
+      .style('text-anchor', 'end');
 
 
   });
-
 })();
