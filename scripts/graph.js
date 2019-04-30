85d9f344a96a22ec46e5525aa63efbbe106930eb
Jeffrey Chase <jc7437a@student.american.edu>
2019-04-30 16:04:51 -0400

    style updates

--- scripts/graph.js
+++ scripts/graph.js
@@ -139,7 +139,12 @@
               let dx = d.target.x - d.source.x,
                 dy = d.target.y - d.source.y,
                 dr = Math.sqrt(dx * dx + dy * dy);
-              return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+//                midx = (dx - dr)/2, midy = (dy - dr)/2;
+            
+              return "M" + d.source.x  + ', ' + d.source.y +
+                " l " + dx + ', ' + dy;
+//              return "M" + d.source.x + ", " + d.source.y + 
+//                " s " + midx + ", " + midy + ' ' + dx + ', ' + dy;
             });
 //
 //      links
