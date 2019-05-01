6b5dceb772f31f702e2a23d9d664f317b83a90c1
Jeffrey Chase <jc7437a@student.american.edu>
2019-05-01 17:18:55 -0400

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,8 @@
       let data = d3.range(10).map(() => {
         return {
           x: Math.random() * width,
-          y: Math.random() * height
+          y: Math.random() * height,
+          r: Math.random() * 60
         }
       });
 
@@ -32,11 +33,11 @@
         .style('opacity', '1')
         .attr('r', 1)
         .transition()
-        .delay((d, i) => i * 30)
+        .delay((d, i) => i * 200)
         .duration(3000)
         .style('fill', 'red')
         .style('opacity', 0)
-        .attr('r', 50)
+        .attr('r', (d)=> d.r)
         .ease(d3.easeLinear)
         .on('end', function (e) {
           d3.select(this).remove()
